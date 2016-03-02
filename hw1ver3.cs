a26007d6b202d2e03957100b26ecae6daaedda44
StarLight <qxm1125@sina.com>
2016-03-02 20:25:10 +0800

    hw

---
+++ hw1ver3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloworld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello C#  " + args.Length);
+            foreach (string x in args)
+            {
+                Console.WriteLine("Test {0}",x);
+            }
+            Console.WriteLine("{0 :G}", System.DateTime.Now);
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+        }
+    }
+}
